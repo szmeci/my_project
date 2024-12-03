b561e1b7f37aee4261677c790dc8cd6f93733b8e
szmeci bbb <kacper.fedeczko@gmail.com>
2024-12-03 16:32:51 +0000

    added blogs support

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,22 +1,23 @@
 use std::cell::RefCell;
-
+use crate::blog::Blog;
 mod blog;
 
 thread_local! {
-    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
+    static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
 // title
 //data
 //tagi
 //komentarze
 
-#[ic_cdk::query]
-fn add_blog(new_blog: String) {
-    BLOGS.with(|blogs| blogs.borrow_mut().push(new_blog));
+#[ic_cdk::update]
+fn add_blog(title: String, date: u32, content: String, tags:Vec<String>) {
+    let blog = Blog::new(title, date, content, tags);
+    BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
 }
 
 #[ic_cdk::query]
-fn get_blogs() -> Vec<String> {
+fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs| blogs.borrow().clone())
 }
 
