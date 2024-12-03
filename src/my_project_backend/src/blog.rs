a8ff455c84de3a5233948981f1ed5450e3f450e5
szmeci bbb <kacper.fedeczko@gmail.com>
2024-12-03 16:45:23 +0000

    added time

--- src/my_project_backend/src/blog.rs
+++ src/my_project_backend/src/blog.rs
@@ -1,18 +1,19 @@
 use candid::CandidType;
+use ic_cdk::api::time;
 
 #[derive(Clone, CandidType)]
 pub struct Blog {
     title: String,
-    date: u32,
+    date: u64,
     content: String,
     tags: Vec<String>
 }
 
 impl Blog {
-    pub fn new(title: String, date: u32, content: String, tags: Vec<String>) -> Self {
+    pub fn new(title: String, content: String, tags: Vec<String>) -> Self {
        Self{
             title,
-            date,
+            date: time(),
             content,
             tags,
        } 
--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -11,8 +11,8 @@ thread_local! {
 //komentarze
 
 #[ic_cdk::update]
-fn add_blog(title: String, date: u32, content: String, tags:Vec<String>) {
-    let blog = Blog::new(title, date, content, tags);
+fn add_blog(title: String, content: String, tags:Vec<String>) {
+    let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
 }
 
