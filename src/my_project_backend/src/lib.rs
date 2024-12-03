2d151dbe4c431cbadf01e6fee0b2edefba0ef49d
szmeci bbb <kacper.fedeczko@gmail.com>
2024-12-03 17:28:08 +0000

    sprawdzanie dlugosci

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -5,15 +5,15 @@ mod blog;
 thread_local! {
     static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
-// title
-//data
-//tagi
-//komentarze
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags:Vec<String>) {
+fn add_blog(title: String, content: String, tags:Vec<String>) -> Result<String, String> {
+    if title.len() > 250 {
+        return Err("title is too long!".to_string())
+    }
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+    Ok("added new blog".to_string())
 }
 
 #[ic_cdk::query]
